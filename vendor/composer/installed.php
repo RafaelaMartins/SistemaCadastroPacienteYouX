cf9675d99d441666520916e9e2c64da51afb1b88
RafaelaMartins <rafaelamartinsv@gmail.com>
2021-07-05 20:14:34 -0300

    save

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '374fad3066bd77a126d67add4e807ffa15d8b4e5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '374fad3066bd77a126d67add4e807ffa15d8b4e5',
+            'dev_requirement' => false,
+        ),
+        'coffeecode/router' => array(
+            'pretty_version' => '1.0.8',
+            'version' => '1.0.8.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../coffeecode/router',
+            'aliases' => array(),
+            'reference' => '29f73e12351a116e9bbb3f695595777a09e18077',
+            'dev_requirement' => false,
+        ),
+    ),
+);
